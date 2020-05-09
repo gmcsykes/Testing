c03ce19d5567bf6efddd9c7916176d8e10efe985
gmcsykes <gmc.sykes@gmail.com>
2020-05-09 11:25:47 +0700

    upto sets

--- hello_world_arcoLinux-3.py
+++ hello_world_arcoLinux-3.py
@@ -88,13 +88,71 @@ menu(drink)
 menu(food)
 
 fruits = ["apple", "mango", "orange"] #list
-numbers = (1, 2,3) #tuple
+numbers = (1, 2, 3) #tuple
 alphabets = {'a':'apple', 'b':'ball', 'c':'cat'} #dictionary
-vowels ={'a', 'e', 'i', 'o', 'i'} #set
+vowels = {'a', 'e', 'i', 'o', 'u'} #set
 
 print(fruits)
 print(numbers)
 print(alphabets)
 print(vowels)
 
+a = 5
+print(a, "is of type", type(a))
 
+a = 2.0
+print(a, "is of type", type(a))
+
+a = 1+2j
+print(a, "is complex number?", isinstance(1+2j,complex))
+
+a = [1, 2.2, 'python']
+
+a = [5,10,15,20,25,30,35,40]
+
+# a[2] = 15
+print("a[2] = ", a[2])
+
+# a[0:3] = [5, 10, 15]]
+print("a[0:3] = ", a[0:3])
+
+# a[5:] = [30, 35, 40]
+print("a[5:] = ", a[5:])
+
+a = [1, 2, 3]
+a[2] = 4
+print(a)
+
+t = (5, 'program', 1+3j)
+
+# t[1] = 'program'
+print("t[1] = ", t[1])
+
+# t[0:3] = (5, 'program', (1+3j))
+print("t[0:3] = ", t[0:3])
+
+#generates an error 
+#Tuples are immutable
+#t[0] = 10
+
+#Python Strings
+
+s = 'Hello World!'
+
+# s[4] = 'o'
+print("s[4] = ", s[4])
+
+# s[6:11] = 'world'
+print("s[6:11] = ", s[6:11])
+
+#Python Set
+
+a = {5,2,3,1,4}
+#printing se variable
+print("a = ", a)
+
+# data type of variable a
+print(type(a))
+
+a = {1,2,2,3,3,3}
+print(a)
