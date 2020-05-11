3ed28c263a1cef7198d2253dcb9789cecc0a84ef
Gregory Sykes <billymakkarom@gmail.com>
2020-05-11 20:46:43 +0700

    test commit from workng copymon ipad

---
+++ exa.py
@@ -0,0 +1,11 @@
+# This sentence is output to the screen
+# Pythin Input Output and Import
+
+a=5
+print('The value of a is', a)
+
+print(1,2,3,4)
+print(1,2,3,4, sep='*')
+#print(1,2,3,4, sep'#', end'&')
+
+
