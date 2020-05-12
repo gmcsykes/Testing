684d5f1eabff79babc2344402492aecc2e6b1564
Gregory Sykes <gmc.sykes@gmail.com>
2020-05-12 09:51:42 +0700

    space added

--- example.py
+++ example.py
@@ -1,11 +1,11 @@
 # Python Module example
 
-def add(a,b):
+def add(a, b):
 """This program adds two
   numbers and returns the 
   result mod on iphone"""
 
 result = a+b
-return result
+  return result
 
 
